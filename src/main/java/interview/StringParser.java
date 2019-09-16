23519841d78d7e92b59f7d0361e05b752fc70456
matthew-laine <matthew.i.laine@gmail.com>
2019-09-16 15:18:55 -0400

    passing test countWordFrequencyOfFooShouldReturnFoo1

--- src/main/java/interview/StringParser.java
+++ src/main/java/interview/StringParser.java
@@ -1,5 +1,25 @@
 package interview;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StringParser {
+
+	public Map<String, Integer> countWordFrequency(String string) {
+		/*
+		 * The basic idea here is going to be using a HashMap to do the work
+		 * If we wanted this to be sortable, we could use a TreeMap and it would
+		 * sort (and do other things) in O(logN) time, but for the purpose of not 
+		 * having it sorted, a HashMap would be faster as accessing entries 
+		 * will be done in Theta(1) time. 
+		 */
+		
+		Map<String, Integer> wordsAndFrequencies = new HashMap<String, Integer>();
+		// Make first test pass.
+		wordsAndFrequencies.put("FOO", 1);
+		return wordsAndFrequencies;
+	}
+	
+	
 
 }
--- src/test/java/interview/StringParserTest.java
+++ src/test/java/interview/StringParserTest.java
@@ -1,5 +1,10 @@
 package interview;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.Map;
+
 import org.junit.Test;
 
 public class StringParserTest {
@@ -16,5 +21,12 @@ public class StringParserTest {
 	@Test
 	public void countWordFrequencyOfFooShouldReturnFoo1 () {
 		StringParser underTest = new StringParser();
+		/*
+		 * The basic idea here is to insert or modify a Key/Value pair
+		 * with the key being every unique capitalized word in the string
+		 * and the value being its frequency.
+		 */
+		Map<String, Integer> fooFrequency = underTest.countWordFrequency("Foo");
+		assertThat(fooFrequency.get("FOO"), is(1));
 	}
 }
