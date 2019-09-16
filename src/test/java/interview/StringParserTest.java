b19fa0cce676ac5c6d3fff64d67d66fe6c44651b
matthew-laine <matthew.i.laine@gmail.com>
2019-09-16 14:57:19 -0400

    test drive creation of StringParser class

--- src/test/java/interview/StringParserTest.java
+++ src/test/java/interview/StringParserTest.java
@@ -1,5 +1,20 @@
 package interview;
 
+import org.junit.Test;
+
 public class StringParserTest {
+	/*
+	 * The StringParser will have a method called countWordFrequency
+	 * The countWordFrequency method will return all the words 
+	 * (delimited by a space) in the string and their frequencies
+	 *
+	 * All returned words should be capitalized so that the words
+	 * "the" "thE" "tHe" "tHE" "The" "ThE" "THe" and "THE"
+	 * are all evaluated as "THE"
+	 */
 	
+	@Test
+	public void countWordFrequencyOfFooShouldReturnFoo1 () {
+		StringParser underTest = new StringParser();
+	}
 }
