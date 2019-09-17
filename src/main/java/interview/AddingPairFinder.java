7daff826df9b5ee9345a146571222186c9055166
matthew-laine <matthew.i.laine@gmail.com>
2019-09-16 21:12:01 -0400

    passing test oneTwoThreeFourFiveShouldGiveTwoPairsThatAddUpToSix by making a method that adds pairs to a list rather than simply returning a single pair, also fix lack of saving with previous commit, so else statement in program logic is now gone

--- src/main/java/interview/AddingPairFinder.java
+++ src/main/java/interview/AddingPairFinder.java
@@ -1,10 +1,11 @@
 package interview;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class AddingPairFinder {
-
 
 	/*
 	 * The getPair method returns the indices of two integers from an array that add
@@ -26,16 +27,43 @@ public class AddingPairFinder {
 			if (numberComplementsAPreviousNumber(input, i, neededNumbers)) {
 				int[] numberPair = makeNumberPair(input, i, neededNumbers);
 				return numberPair;
-			}
-			else {
-				neededNumbers.put(targetSum-input[i], i);
+			} 
+			if (!numberComplementsAPreviousNumber(input, i, neededNumbers)){
+				neededNumbers.put(targetSum - input[i], i);
 			}
 		}
 		return pairDoesNotExist;
 	}
 
+	/*
+	 * Here, the idea is to return a list of all the indices of the pairs of numbers
+	 * that add up to the target sum. We will add the pairs to a list rather than
+	 * having a return statement within the loop as done in our previous method.
+	 */
+	public List<int[]> getPairs(int[] input, int targetSum) {
+
+		int[] pairDoesNotExist = { -1, -1 };
+
+		Map<Integer, Integer> neededNumbers = new HashMap<Integer, Integer>();
+		List<int[]> pairs = new ArrayList<int[]>();
+
+		for (int i = 0; i < input.length; i++) {
+			if (numberComplementsAPreviousNumber(input, i, neededNumbers)) {
+				int[] numberPair = makeNumberPair(input, i, neededNumbers);
+				pairs.add(numberPair);
+			} 
+			if (!numberComplementsAPreviousNumber(input, i, neededNumbers)) {
+				neededNumbers.put(targetSum - input[i], i);
+			}
+		}
+		if (pairs.size() == 0) {
+			pairs.add(pairDoesNotExist);
+		}
+		return pairs;
+	}
+
 	private int[] makeNumberPair(int[] input, int i, Map<Integer, Integer> neededNumbers) {
-		int[] numberPair = {neededNumbers.get(input[i]), i};
+		int[] numberPair = { neededNumbers.get(input[i]), i };
 		return numberPair;
 	}
 
--- src/test/java/interview/AddingPairFinderTest.java
+++ src/test/java/interview/AddingPairFinderTest.java
@@ -3,31 +3,49 @@ package interview;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class AddingPairFinderTest {
-	
-	int[] oneTwo = {1,2};
-	
+
+	int[] oneTwo = { 1, 2 };
+	int[] oneThruFive = { 1, 2, 3, 4, 5 };
+
 	AddingPairFinder underTest;
-	
+
 	@Before
-	public void setup () {
+	public void setup() {
 		underTest = new AddingPairFinder();
 	}
-	
+
 	@Test
-	public void oneAndTwoShouldNotAddUpToFourInsteadShouldReturnNegOneNegOne () {
+	public void oneAndTwoShouldNotAddUpToFourInsteadShouldReturnNegOneNegOne() {
 		int[] pair = underTest.getPair(oneTwo, 4);
 		assertThat(pair[0], is(-1));
 		assertThat(pair[1], is(-1));
 	}
-	
+
 	@Test
-	public void oneAndTwoShouldAddUpToThreeShouldReturnZeroOne () {
+	public void oneAndTwoShouldAddUpToThreeShouldReturnZeroOne() {
 		int[] pair = underTest.getPair(oneTwo, 3);
 		assertThat(pair[0], is(0));
 		assertThat(pair[1], is(1));
+	}
+
+	@Test
+	public void oneTwoThreeFourFiveShouldGiveTwoPairsThatAddUpToSix() {
+		List<int[]> pairs = underTest.getPairs(oneThruFive, 6);
+		/*
+		 * The reason these assertions are ordered the way they are is that the first
+		 * pair {1,3} will be made when you hit element 3 of the array, and the second
+		 * pair {2,4} will be made when you hit element 4 of the array.
+		 */
+		assertThat(pairs.get(0)[0], is(1));
+		assertThat(pairs.get(0)[1], is(3));
+		assertThat(pairs.get(1)[0], is(0));
+		assertThat(pairs.get(1)[1], is(4));
+
 	}
 }
