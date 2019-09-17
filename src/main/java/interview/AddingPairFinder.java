7c9e330970bf92cdbfc232afca96bab6a9b84dc6
matthew-laine <matthew.i.laine@gmail.com>
2019-09-16 20:40:03 -0400

    passing test oneAndTwoShouldAddUpToThreeShouldReturnZeroOne by implementing a HashMap.

--- src/main/java/interview/AddingPairFinder.java
+++ src/main/java/interview/AddingPairFinder.java
@@ -1,10 +1,37 @@
 package interview;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AddingPairFinder {
 
+
+	/*
+	 * The getPair method returns the indices of two integers from an array that add
+	 * up to the target sum. If no such numbers exist, then the return is {-1, -1}.
+	 */
 	public int[] getPair(int[] input, int targetSum) {
-		int[] pair = {-1,-1};
-		return pair;
+		/*
+		 * Here, the idea is to find a data structure that will quickly allow us to
+		 * determine whether a certain number in our array has a pair. We do this by
+		 * making a HashMap of numbers needed to add to each array element to reach the
+		 * target sum. We can then quickly access the HashMap to see whether we have a
+		 * match as we iterate through the array.
+		 */
+		int[] pairDoesNotExist = { -1, -1 };
+
+		Map<Integer, Integer> neededNumbers = new HashMap<Integer, Integer>();
+
+		for (int i = 0; i < input.length; i++) {
+			if (neededNumbers.containsKey(input[i])) {
+				int[] numberPair = {neededNumbers.get(input[i]), i};
+				return numberPair;
+			}
+			else {
+				neededNumbers.put(targetSum-input[i], i);
+			}
+		}
+		return pairDoesNotExist;
 	}
 
 }
--- src/test/java/interview/AddingPairFinderTest.java
+++ src/test/java/interview/AddingPairFinderTest.java
@@ -3,18 +3,31 @@ package interview;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AddingPairFinderTest {
 	
 	int[] oneTwo = {1,2};
 	
+	AddingPairFinder underTest;
+	
+	@Before
+	public void setup () {
+		underTest = new AddingPairFinder();
+	}
+	
 	@Test
 	public void oneAndTwoShouldNotAddUpToFourInsteadShouldReturnNegOneNegOne () {
-		AddingPairFinder underTest = new AddingPairFinder();
 		int[] pair = underTest.getPair(oneTwo, 4);
 		assertThat(pair[0], is(-1));
 		assertThat(pair[1], is(-1));
 	}
-
+	
+	@Test
+	public void oneAndTwoShouldAddUpToThreeShouldReturnZeroOne () {
+		int[] pair = underTest.getPair(oneTwo, 3);
+		assertThat(pair[0], is(0));
+		assertThat(pair[1], is(1));
+	}
 }
