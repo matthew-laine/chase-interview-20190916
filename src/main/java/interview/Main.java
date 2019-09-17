07b2ec05bfa15ce303906565197560b86ed05038
matthew-laine <matthew.i.laine@gmail.com>
2019-09-16 22:34:53 -0400

    modify main method to print all integer pairs from 0 to 99 that sum to 100

--- src/main/java/interview/Main.java
+++ src/main/java/interview/Main.java
@@ -1,10 +1,22 @@
 package interview;
 
+import java.util.List;
+
 public class Main {
 	public static void main(String[] args) {
 		WordCounter parser = new WordCounter();
 		System.out.println(
 				parser.countWordFrequency("Happy days are here again. Happy happy happy. Why are happy days here?"));
 
+		AddingPairFinder finder = new AddingPairFinder();
+		int[] numbers = new int[100];
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = i;
+		}
+		List<int[]> pairsThatSumTo100 = finder.getPairs(numbers, 100);
+		for (int i = 0; i < pairsThatSumTo100.size(); i++) {
+			int[] pair = pairsThatSumTo100.get(i);
+			System.out.println(pair[0] + ", " + pair[1]);			
+		}
 	}
 }
